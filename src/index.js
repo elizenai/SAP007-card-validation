539717e545d29051c7b34c41a4db5b10fa100a0f
Elizenai Silva <elizenai.silva@hotmail.com>
2022-02-01 21:46:32 -0300

    inclusão do index.js e validator.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import validator from './validator.js';
+//console.log(validator);
 
-console.log(validator);
+const button = document.getElementById("validar");
+
+function validando(e) {
+    e.preventDefault()
+
+    let ncard = parseInt(document.getElementById("numeroCartao")).value;  
+    const validarcard = validator.isValid(numeroCartao);
+    document.getElementById("resposta").innerHTML = validarcard;
+}
+
+button.addEventListener("click", validando);
+
+
+
+
+
+
+
+
+
+/*const valorInput = document.getElementById("numeroCartao"); 
+function validando(e) {
+    e.preventDefault()
+
+    let ncard = valorInput.value;
+    const validarCard = validator.isValid(ncard);
+    let resposta = document.getElementById("resposta").value;
+    if (validarCard === true) {
+        resposta.innerHTML = "CARTÃO VÁLIDO"; //+ mask;
+    }else{
+        resposta.innerHTML = "CARTÃO INVÁLIDO"; //+ mask;
+    }
+    const button = document.getElementById("validar");
+    button.addEventListener("click", validando)
+}*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,35 @@
 const validator = {
-  // ...
-};
+
+    isValid(numeroCartao) {
+      const card = Array.from(numeroCartao) 
+      let cardRev = card.reverse() 
+      let soma = 0
+    
+
+      //vai percorrer todo o numero
+        for (let i= 0; i <= cardRev.length; i++) {
+
+            if (i % 2 != 0){
+             cardRev[i] = (Number(cardRev[i])) * 2
+                
+             if (cardRev[i] < 9 ){
+                 cardRev[i] = cardRev[i] - 9
+                 soma += (Number(cardRev[i]))
+             }else{
+                 soma += (cardRev[i])
+             }
+            }else{
+                soma += (Number(cardRev[i]))
+            }
+        }
+        if (soma % 10 ===  0){
+            return numeroCartao = true
+        }else{
+            return numeroCartao = false
+        }
+            
+        
+    }
+}
 
 export default validator;
